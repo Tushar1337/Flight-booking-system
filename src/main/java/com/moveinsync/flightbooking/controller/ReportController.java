e5adccfd0a74f2f602c0add9f391bde5e90ba2b0
Hardik Garg <Hardik12c@gmail.com>
2023-03-28 10:20:59 +0530

    pdf generator added

--- src/main/java/com/moveinsync/flightbooking/controller/ReportController.java
+++ src/main/java/com/moveinsync/flightbooking/controller/ReportController.java
@@ -1,17 +1,32 @@
 package com.moveinsync.flightbooking.controller;
 
+import com.itextpdf.text.DocumentException;
 import com.moveinsync.flightbooking.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
 
 @RestController
 public class ReportController {
     @Autowired
     ReportService reportService;
-    @GetMapping("/generatereport/{flightNumber}")
-    public String generatereports(@PathVariable("flightNumber") String  flightNumber){
-        return reportService.generateReports(flightNumber);
+    @GetMapping("/generatereport")
+    public ResponseEntity<InputStreamResource> generatereports() throws DocumentException {
+
+        ByteArrayInputStream bis=reportService.generateReports();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=my-pdf.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bis));
     }
 }
