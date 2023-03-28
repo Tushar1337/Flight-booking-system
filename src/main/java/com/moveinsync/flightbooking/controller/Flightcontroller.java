3dc60d082c55ab56719721fa46929d3140f2d1c7
Tushar Raina <tushar.raina@moveinsync.com>
2023-03-28 12:26:00 +0530

    jwt changes

--- src/main/java/com/moveinsync/flightbooking/controller/Flightcontroller.java
+++ src/main/java/com/moveinsync/flightbooking/controller/Flightcontroller.java
@@ -26,11 +26,18 @@ public class Flightcontroller {
         return flightservice.getallseatsbyflightid(flightid);
     }
     @GetMapping("/getseatsrelatedtouser")
-    public List<FlightSeat> getseatsrelatedtouser(){
-        return flightservice.getseatsrelatedtouser();
+    public List<FlightSeat> getseatsrelatedtouser(@RequestHeader Map request){
+        String token = request.get("authorization").toString().substring(7);
+        return flightservice.getseatsrelatedtouser(token);
     }
     @PostMapping("/{seatid}")
-    public String booktheseat(@PathVariable("seatid") Long seatid) {
-        return flightservice.bookaseat(seatid);
+    public String booktheseat(@PathVariable("seatid") Long seatid, @RequestHeader Map request) {
+        String token = request.get("authorization").toString().substring(7);
+        return flightservice.bookaseat(seatid, token);
+    }
+    @DeleteMapping("/{seatid}")
+    public String cancelbooking(@PathVariable("seatid") Long seatid, @RequestHeader Map request) {
+        String token = request.get("authorization").toString().substring(7);
+        return flightservice.deleteaseat(seatid, token);
     }
 }
